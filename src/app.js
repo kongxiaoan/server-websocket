2021d88b38d5b0ebf359eb7b65173fbab07ebd86
kpa <billkp@yeah.net>
2023-03-17 15:59:19 +0800

    添加pb 解析

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const middleware = require('./services/server');
 const routes = require('./api/routes');
-const createWebSocketServer = require('./services/websocket');
+const websocket = require('./services/websocket');
 
 const app = express();
-const port = process.env.PORT || 8080; // 默认端口号为 8080
+const port = process.env.PORT || 9000; // 默认端口号为 8080
 
 app.use(middleware);
 app.use('/api', routes);
 
-const wss = createWebSocketServer();
+websocket.start_websocket();
 
 app.listen(port, () => console.log(`应用程序已启动，监听端口 ${port}`));
--- src/services/websocket.js
+++ src/services/websocket.js
@@ -1,67 +1,78 @@
 const WebSocket = require('ws');
+const protobuf = require("protobufjs");
+const protoFile = "../public/IMClientParams.proto";
 
-// 存储已验证的客户端
-const clients = new Set();
-
-// 发送心跳包
-function heartbeat() {
-  clients.forEach((client) => {
-    if (!client.isAlive) return client.terminate();
-    client.isAlive = false;
-    client.ping(null, undefined);
-  });
-}
-
-function createWebSocketServer() {
-    console.log('开始启动websocket')
-    const wss = new WebSocket.Server({ port: 8087 });
-
-    wss.on('connection', function connection(ws) {
-      console.log('WebSocket connected');
-    
-      ws.isAlive = true;
-        // 处理收到的消息
-      ws.on('message', (message) => {
-        JSON.stringify(message)
-        if (ws.isAuthorized) {
-            if(message == 'pang') {
-                // pang 
+function start_websocket() {
+    (async () => {
+        const root = await protobuf.load(protoFile);
+        
+        // 生成解析器
+        const parser = root.lookupType("com.example.mylibrary.IMClientParams");
+        console.log("websocket 已启动")
+        const wss = new WebSocket.Server({ port: 8080 });
+        
+        // 存储已验证的客户端
+        const clients = new Set();
+        
+        // 发送心跳包
+        function heartbeat() {
+          clients.forEach((client) => {
+            if (!client.isAlive) return client.terminate();
+            client.isAlive = false;
+            client.ping(null, undefined);
+          });
+        }
+        
+        wss.on('connection', (ws) => {
+          console.log('Client connected');
+          ws.isAlive = true;
+          // 处理收到的消息
+          ws.on('message', (message) => {
+            if (ws.isAuthorized) {
+                if(message == 'pang') {
+                    // pang
+                } else {
+                    const _message = parser.decode(message);
+                    const jsonMessage = _message.toJSON();
+                    console.log(jsonMessage);
+                    ws.send("我是服务端，收到了你的消息")
+                }
+        
             } else {
-                console.log(`Received message from authorized client: ${message}`);
+              if (message == 'auth') {
+                ws.isAuthorized = true;
+                clients.add(ws);
+                console.log('Client authorized');
+                ws.send(`authorized`)
+              } else {
+                console.log('Client unauthorized');
+                ws.terminate();
+              }
             }
-    
-        } else {
-        if (message == 'auth') {
-            ws.isAuthorized = true;
-            clients.add(ws);
-            console.log('Client authorized');
-            ws.send(`authorized`)
-        } else {
-            console.log('Client unauthorized');
-            ws.terminate();
-        }
-        }
-    });
-    ws.on('error', e => {
-        console.log(e)
-        ws.send(e)
-    })
+          });
+        
+          ws.on('error', e => {
+            console.log(e)
+            ws.send(e)
+          })
+        
+          // 处理连接关闭
+          ws.on('close', () => {
+            console.log("已经断开连接")
+            clients.delete(ws);
+          });
+        
+          // 处理心跳检测
+          ws.on('pong', () => {
+            ws.isAlive = true;
+          });
+        });
+        
+        // 启动心跳检测
+        setInterval(heartbeat, 30000);
+        })(); 
+};
 
-    // 处理连接关闭
-    ws.on('close', () => {
-        console.log('Client disconnected');
-        clients.delete(ws);
-     });
-
-    // 处理心跳检测
-    ws.on('pong', () => {
-        ws.isAlive = true;
-    });
-    });
-
-
-    // 启动心跳检测
-    setInterval(heartbeat, 30000);
-    return wss;
+module.exports = {
+    start_websocket: start_websocket
 }
-module.exports = createWebSocketServer
